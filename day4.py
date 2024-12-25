9ab5f841d6ae698ef633bd6262ed538e7b447442
Beeya <beafandre@gmail.com>
2024-12-25 11:00:32 +0000

    Day 4 - Rock paper scissors

---
+++ day4.py
@@ -0,0 +1,30 @@
+import random
+
+print("Rock, paper or scissors?")
+choice = input()
+player = -1
+
+# [0] Rock
+# [1] Paper
+# [2] Scissors
+
+match choice:
+    case "rock":
+        player = 0
+    case "paper":
+        player = 1
+    case "scissors":
+        player = 2
+        
+cpu = random.randint(0, 2)
+
+if player > cpu or (player == 0 and cpu == 2):
+    win = "YOU WON!"
+elif player == cpu:
+    win = "YOU TIED!"
+else:
+    win = "YOU LOST!"
+    
+moves = ["rock", "paper", "scissors"]
+print(f"You played {choice} and the computer played {moves[cpu]}.")
+print(win)
